54ff5b8bcf66bea65440e620e8f932fbe9de08f8
robsant <43965991+robsant@users.noreply.github.com>
2026-02-23 20:59:38 +0100

    Initial Project

--- SpendSmart/Controllers/HomeController.cs
+++ SpendSmart/Controllers/HomeController.cs
@@ -18,6 +18,10 @@ namespace SpendSmart.Controllers
         {
             return View();
         }
+        public IActionResult CreateEditExpense()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
